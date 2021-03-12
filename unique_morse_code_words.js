3c5a9ae4ce46c91a90e505640285fcbfbeef03c0
Linh <lhvo2112@gmail.com>
2021-03-12 18:39:07 -0500

    Completed unique morse code words

--- unique_morse_code_words.js
+++ unique_morse_code_words.js
@@ -3,6 +3,27 @@
  * @return {number}
  */
 var uniqueMorseRepresentations = function (words) {
-
     morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+    /**
+     * convert character in each word to ASCII number - 97 to match with index of morse code
+     * get morse code from array and concat it to transform the word
+     * add the morse code to a set
+     * return size of set
+     */
+
+    let diffCodes = new Set();
+
+    for (let word of words) {
+        let transformed = '';
+
+        for (let char of word) {
+            let idx = char.codePointAt() - 97;
+            transformed += morseCode[idx];
+        }
+
+        diffCodes.add(transformed);
+    }
+
+    return diffCodes.size;
 };
